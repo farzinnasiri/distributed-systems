23bf7896b3d6922c02bac218448f26499596ebf6
farzinnasiri <farzinnasiri890@gmail.com>
2020-03-01 15:41:39 +0330

    hw0 added

---
+++ hw0/main.py
@@ -0,0 +1,23 @@
+def main():
+    class server():
+        def __init__(self):
+            pass
+
+    class client():
+        def __init__(self):
+            pass
+
+    sr, ip1, port1, ip2, port2 = input().split()
+    print(sr,ip1,port1,ip2,port2)
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
