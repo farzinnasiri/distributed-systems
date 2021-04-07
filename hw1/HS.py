ef46b461c7cd30b920c745fc3b715a89c1936aec
farzin nasiri <farzin@carriot.ir>
2021-04-07 16:46:37 +0430

    implemention completed

--- hw1/HS.py
+++ hw1/HS.py
@@ -5,8 +5,15 @@ from socket import *
 import json
 import threading
 
+lock = threading.Lock()
 
-# Message class for storing messages
+
+def log(*args):
+    lock.acquire()
+    print(time.strftime("%I:%M:%S %p", time.localtime()), " ", *args)
+    lock.release()
+
+
 class Token:
     def __init__(self, src_pid, type_, direction, max_hop_count, hop_count):
         self.src_pid = src_pid
@@ -45,6 +52,7 @@ class Node(threading.Thread):
         self.leader_pid = None
 
         self.round = 0
+        self.current_rounds_replies = 0
 
         self.receive_socket = socket(AF_INET, SOCK_DGRAM)
 
@@ -57,12 +65,20 @@ class Node(threading.Thread):
         self.send_right_socket = socket(AF_INET, SOCK_DGRAM)
 
     def send_to_left(self, token: Token):
-        self.send(channel=self.send_left_socket, other_node=self.left_node, token=token)
+        token.direction = "CLOCKWISE"
+        threading.Thread(target=self.send(channel=self.send_left_socket, other_node=self.left_node, token=token)) \
+            .start()
 
     def send_to_right(self, token: Token):
-        self.send(channel=self.send_right_socket, other_node=self.right_node, token=token)
+        token.direction = "ANTICLOCKWISE"
+        threading.Thread(target=self.send(channel=self.send_right_socket, other_node=self.right_node, token=token)) \
+            .start()
 
     def send(self, channel: socket, other_node: NeighbourNode, token: Token):
+        log(self.pid, ": sending a token of type ", token.type_,
+            " from ", token.src_pid, " in ",
+            token.direction, "direction")
+
         time.sleep(self.channel_delay)
         channel.sendto(json.dumps(token.__dict__).encode("utf-8"), (other_node.ip, other_node.port))
 
@@ -83,80 +99,135 @@ class Node(threading.Thread):
         if self.is_active:
             token.type_ = "REPLY"
             if token.direction == "CLOCKWISE":
-                token.direction = "ANTICLOCKWISE"
                 self.send_to_right(token)
 
             elif token.direction == "ANTICLOCKWISE":
-                token.direction = "CLOCKWISE"
                 self.send_to_left(token)
 
     def run(self):
-        print("process ", self.pid, "starting...")
+        log("process ", self.pid, " starting...")
 
         if not self.is_active:
+
             self.receive_socket.bind((self.receive_ip, self.receive_port))
-            self.probe(token=Token(src_pid=self.pid, type_="PROBE", max_hop_count=(2 ** self.round), hop_count=0))
+            self.is_active = True
+
+            self.probe(token=Token(src_pid=self.pid, type_="PROBE", direction=None, max_hop_count=(2 ** self.round),
+                                   hop_count=0))
 
         while self.is_active:
+            # listening for incoming tokens
             payload, _ = self.receive_socket.recvfrom(2048)
-            self.is_active = self.transition(data=payload)
+            self.transition(data=payload)
 
-        self.receive_socket.close()
+        self.end_process()
 
     def transition(self, data):
         if data == b'':
             return False
-        # print("================================")
-        # print("receiving message at ", time.strftime("%I:%M:%S %p", time.localtime()))
 
-        message = Token(**json.loads(data))
+        token = Token(**json.loads(data))
+        log(self.pid, ": a token of type ", token.type_,
+            " received from ", token.src_pid, " in ", token.direction, " direction")
 
-        print("payload: ", message.type, " ", message.value)
-        print("================================")
-        if message.type == "start" and message.value == "hello":
-            self.send_message("start", "hi")
-            return True
-        elif message.type == "start" and message.value == "hi":
-            self.send_message("end", "goodbye")
-            return True
-        elif message.type == "end" and message.value == "goodbye":
-            self.send_message("end", "bye")
-            return True
-        elif message.type == "end" and message.value == "bye":
-            self.send_socket.close()
-            return False
+        token.hop_count += 1
+
+        if token.type_ == "PROBE":
+            if token.src_pid == self.pid:  # leader is found
+                # process should announces itself as the leader to other nodes
+                if not (self.is_leader or self.is_leader_known):
+                    self.is_leader = True
+                    self.is_leader_known = True
+                    self.leader_pid = self.pid
+
+                    log("process ", self.pid, ": leader is ",
+                        self.leader_pid, " ANNOUNCING the leader now")
+
+                    self.send_to_right(Token(src_pid=self.pid, type_="ANNOUNCEMENT",
+                                             direction=None,
+                                             max_hop_count=2 ** self.round,
+                                             hop_count=0))
+            elif token.src_pid > self.pid:
+                if token.hop_count < token.max_hop_count:
+                    self.relay_forward(token)
+                else:
+                    token.type_ = "REPLY"
+                    self.reply(token)
+            else:
+                # token is discarded
+                pass
+
+        elif token.type_ == "REPLY":
+            if token.src_pid != self.pid:
+                self.relay_forward(token)
+            else:  # a REPLY from a previous probing is returned
+                self.current_rounds_replies += 1
+                if self.current_rounds_replies == 2:  # process goes to the next round (two tokens have arrived)
+                    self.current_rounds_replies = 0
+                    self.round += 1
+                    self.probe(
+                        token=Token(src_pid=self.pid, type_="PROBE",
+                                    direction=None,
+                                    max_hop_count=(2 ** self.round),
+                                    hop_count=0))
+
+        elif token.type_ == "ANNOUNCEMENT":
+            if self.pid != token.src_pid and not self.is_leader_known:
+                # a NON-LEADER process identifies the leader
+
+                self.is_leader_known = True
+                self.leader_pid = token.src_pid
+                self.relay_forward(token)
+
+                log(self.pid, ": leader is ",
+                    self.leader_pid)
+
+            elif self.pid == token.src_pid and self.is_leader:
+                # the LEADER gets its announcement back which means all other processes now know the leader
+
+                log("election completed with process ",
+                    self.leader_pid, " elected as the leader")
+
+            self.is_active = False
+
+    def end_process(self):
+        log("process ", self.pid, " stopping...")
+        time.sleep(5)
+        self.receive_socket.close()
 
 
 def main():
-    # Node class contains two sockets: receive and send
-    # The first one listens for messages and the second one sends them
-    # If the Node is the initializer of the conversation then it binds the sockets with to ports
-    # otherwise the node just connects to the sockets
-    # Every node is on a separate thread waiting for a message to reply
+    n = int(input())
 
-    # getting input form
-    sr, ip1, port1, ip2, port2 = input().split()
+    nodes = []
 
-    if sr == "r":
-        node = Node(ip1, int(port1), ip2, int(port2))
-        node.bind()
-        print("process starting as receiver...")
+    port = 8000
+
+    for i in range(n):
+        pid, delay = input().split()
+        temp_port = port + i
+
+        node = Node(int(pid), "localhost", temp_port, int(delay))
+
+        nodes.append(node)
+
+    # setting the right/left neighbours of each node to establish connection
+    for i, node in enumerate(nodes):
+        right_port = port + (i + n - 1) % n
+        left_port = port + (i + 1) % n
+
+        node.set_right_node("localhost", right_port)
+        node.set_left_node("localhost", left_port)
+
+    print(time.strftime("%I:%M:%S %p", time.localtime()), " Starting Election...")
+    for node in nodes:
         node.start()
 
-    elif sr == "s":
-        node = Node(ip1, int(port1), ip2, int(port2))
-        node.connect()
-        print("process starting as sender...")
-        node.start()
-        node.send_message("start", "hello")
+    for node in nodes:
+        node.join()
 
-    else:
-        print("Invalid type identifier")
+    print(time.strftime("%I:%M:%S %p", time.localtime()), "Exiting program...")
 
 
 if __name__ == "__main__":
     main()
-
-# First start the receiver than the sender
-# to start a process as a receiver : r 127.0.0.1 54329 127.0.0.1 54328
-# to start a process as a sender : s 127.0.0.1 54328 127.0.0.1 54329
