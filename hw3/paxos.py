8c5767a2107b56b652c20cf2ef12a23a3cc87b50
farzin nasiri <farzin@carriot.ir>
2021-05-27 00:09:53 +0430

    finished

--- hw3/paxos.py
+++ hw3/paxos.py
@@ -25,9 +25,9 @@ POTENTIAL_LEADER_ACK = "POTENTIAL_LEADER_ACK"
 V_PROPOSE_ACK = "V_PROPOSE_ACK"
 
 
-def log(pid: int, *args):
+def log(nid: int, *args):
     lock.acquire()
-    print(time.strftime("%I:%M:%S %p ", time.localtime()), "process ", pid, ": ", *args)
+    print(time.strftime("%I:%M:%S %p ", time.localtime()), "node ", nid, ": ", *args)
     lock.release()
 
 
@@ -72,22 +72,22 @@ class Node(threading.Thread):
         self.phase1_timer = phase1_timer
         self.phase2_timer = phase2_timer
 
-        self.leader_id = self.id
-        self.max_leader_id_proposed = self.leader_id
+        self.leader_id = 0
 
         self.proposing_value = -1
         self.phase = 0
 
-        self.is_decided = False
-        self.proposed_value = -1
+        self.leader_ids = [0]
+        self.value_proposed_leader_ids = []
+
+        self.chosen_value = -1
 
         self.count_promises = 0
         self.count_accepts = 0
 
-        self.receive_socket = socket(AF_INET, SOCK_DGRAM)
+        self.is_decided = False
 
-    def set_network_size(self, n: int):
-        self.network_size = n
+        self.receive_socket = socket(AF_INET, SOCK_DGRAM)
 
     def add_neighbour(self, node: NeighbourNode):
         self.neighbours.append(node)
@@ -98,23 +98,18 @@ class Node(threading.Thread):
 
         for neighbour in self.neighbours:
             threading.Thread(
-                target=self.send_to_node, args=(neighbour.channel,
-                                                neighbour, message)).start()
+                target=self.send_to_node, args=(neighbour, message)).start()
 
-    def send_to_node(self, channel: socket, receiver_node: NeighbourNode, message: Message):
+    def send_to_node(self, receiver_node: NeighbourNode, message: Message):
         log(self.id, "sending a message of type", message.type_,
             "with value", message.value,
             "to", receiver_node.id)
 
         time.sleep(receiver_node.delay)
-        channel.sendto(json.dumps(message.__dict__).encode("utf-8"),
-                       (receiver_node.ip, receiver_node.port))
+        receiver_node.channel.sendto(json.dumps(message.__dict__).encode("utf-8"),
+                                     (receiver_node.ip, receiver_node.port))
 
-    def send_by_id(self, receiver_node_id: int, message: Message):
-        log(self.id, "sending a message of type", message.type_,
-            "with value", message.value,
-            "to", receiver_node_id)
-
+    def send_to(self, receiver_node_id: int, message: Message):
         receiver_node = None
 
         for neighbour in self.neighbours:
@@ -122,9 +117,8 @@ class Node(threading.Thread):
                 receiver_node = neighbour
                 break
 
-        time.sleep(receiver_node.delay)
-        receiver_node.channel.sendto(json.dumps(message.__dict__).encode("utf-8"),
-                                     (receiver_node.ip, receiver_node.port))
+        threading.Thread(
+            target=self.send_to_node, args=(receiver_node, message)).start()
 
     def run(self):
         log(self.id, "starting...")
@@ -144,16 +138,18 @@ class Node(threading.Thread):
         self.stop()
 
     def prepare(self):
-        if self.is_active and self.state == UNKNOWN:
+        if self.is_active and self.state == POTENTIAL_LEADER:
 
             time.sleep(self.startup_delay)
 
-            if self.is_active and self.state == POTENTIAL_LEADER:
-                self.leader_id += 1
+            if self.is_active and not self.is_decided:
+                self.state = POTENTIAL_LEADER
+
+                self.leader_id = max(self.leader_ids) + 1
                 self.count_promises += 1
 
                 self.broadcast(Message(nid=self.id,
-                                       type_=PREPARE,
+                                       type_=POTENTIAL_LEADER,
                                        value=self.leader_id))
 
                 time.sleep(self.phase1_timer)
@@ -163,25 +159,32 @@ class Node(threading.Thread):
 
     def propose(self):
         if self.is_active and self.state == LEADER:
-            if self.proposing_value == -1:
-                self.proposing_value = self.id * self.network_size
+            if len(self.value_proposed_leader_ids) == 0:
+                self.chosen_value = self.id * self.network_size
+            else:
+                self.chosen_value = \
+                    max(self.value_proposed_leader_ids,
+                        key=lambda i: i[0])[1]
 
             self.count_accepts += 1
 
             self.broadcast(Message(nid=self.id,
                                    type_=V_PROPOSE,
                                    value="%s,%s" % (self.leader_id,
-                                                    self.proposing_value)))
+                                                    self.chosen_value)))
             time.sleep(self.phase2_timer)
 
-            if self.is_active and self.state != LEADER:
+            if self.is_active:
                 self.state = ACCEPTOR
 
     def decide(self):
         if self.is_active and self.state == LEADER and self.is_decided:
+            log(self.id, "decided on value ", self.chosen_value)
             self.broadcast(Message(nid=self.id,
                                    type_=V_DECIDE,
-                                   value=self.proposed_value))
+                                   value=self.chosen_value))
+            self.is_active = False
+            self.stop()
 
     def transition(self, data):
         if data == b'':
@@ -194,46 +197,71 @@ class Node(threading.Thread):
             "with value of", message.value,
             "from", message.nid)
 
-        if message.type_ == PREPARE:
+        if message.type_ == POTENTIAL_LEADER:
 
             leader_id = int(message.value)
 
-            if leader_id > self.leader_id:
-                if self.state != ACCEPTOR:
-                    self.state = ACCEPTOR
-                    self.leader_id = leader_id
+            if leader_id > self.leader_id and (self.state == LEADER or self.state == POTENTIAL_LEADER):
+                self.state = ACCEPTOR
 
-                if self.proposed_value == -1:
-                    threading.Thread(
-                        target=self.send_by_id,
-                        args=(message.nid,
-                              Message(nid=self.id,
-                                      type_=POTENTIAL_LEADER_ACK,
-                                      value="0,-1")
-                              )).start()
-                else:
-                    threading.Thread(
-                        target=self.send_by_id,
-                        args=(message.nid, Message(nid=self.id,
-                                                   type_=POTENTIAL_LEADER_ACK,
-                                                   value="%s,%s" % (
-                                                       self.max_leader_id_proposed,
-                                                       self.proposed_value
-                                                   )))).start()
+            if leader_id > max(self.leader_ids) and self.state == ACCEPTOR:
+                self.leader_ids.append(leader_id)
+
+                max_leader_id_proposed, proposed_value = 0, -1
+
+                if len(self.value_proposed_leader_ids) > 0:
+                    max_leader_id_proposed, proposed_value = \
+                        max(self.value_proposed_leader_ids,
+                            key=lambda i: i[0])[0:]
+
+                self.send_to(message.nid,
+                             Message(nid=self.id,
+                                     type_=POTENTIAL_LEADER_ACK,
+                                     value="%s,%s" % (
+                                         max_leader_id_proposed,
+                                         proposed_value
+                                     )))
+
+        if message.type_ == V_PROPOSE and self.state == ACCEPTOR:
+            leader_id, proposed_value = [int(x) for x in message.value.split(",")]
+
+            self.value_proposed_leader_ids.append(tuple((leader_id, proposed_value)))
+
+            if leader_id == max(self.leader_ids):
+                self.chosen_value = proposed_value
+
+                self.send_to(message.nid,
+                             Message(nid=self.id,
+                                     type_=V_PROPOSE_ACK,
+                                     value="-1"))
+
+        if message.type_ == V_DECIDE:
+            chosen_value = int(message.value)
+            self.chosen_value = chosen_value
+
+            log(self.id, "decided on value ", self.chosen_value)
+            self.is_decided = True
+            self.is_active = False
 
         if message.type_ == POTENTIAL_LEADER_ACK and self.state == POTENTIAL_LEADER:
             self.count_promises += 1
 
-            if self.count_promises >= math.ceil(self.network_size / 2) and self.state == POTENTIAL_LEADER:
-                self.state = LEADER
+            max_leader_id_proposed, proposed_value = [int(x) for x in message.value.split(",")]
 
+            if proposed_value != -1:
+                self.value_proposed_leader_ids.append(tuple((max_leader_id_proposed, proposed_value)))
+
+            if self.count_promises >= math.floor(self.network_size / 2) + 1 and self.state == POTENTIAL_LEADER:
+                self.state = LEADER
                 threading.Thread(target=self.propose).start()
 
         if message.type_ == V_PROPOSE_ACK and self.state == LEADER:
             self.count_accepts += 1
 
-            if self.count_accepts >= math.ceil(self.network_size / 2) and self.state == LEADER:
+            if not self.is_decided and self.count_accepts >= math.floor(
+                    self.network_size / 2) + 1 and self.state == LEADER:
                 self.is_decided = True
+                threading.Thread(target=self.decide).start()
 
     def stop(self):
         log(self.id, "stopping...")
@@ -244,7 +272,7 @@ class Node(threading.Thread):
 def main():
     nodes = []
 
-    port = 8000
+    port = 9000
 
     n = int(input())
 
@@ -254,7 +282,7 @@ def main():
             [float(x) for x in input().split()]
         nid = int(nid)
 
-        node = Node(nid, "localhost", n, port + nid, startup_delay,
+        node = Node(nid, "localhost", port + nid, n, startup_delay,
                     phase1_timer, phase2_timer)
 
         for j in range(n - 1):
@@ -265,7 +293,7 @@ def main():
 
         nodes.append(node)
 
-    print(time.strftime("%I:%M:%S %p", time.localtime()), " Starting MIS...")
+    print(time.strftime("%I:%M:%S %p", time.localtime()), " Starting PAXOS...")
 
     # for node in nodes:
     #     for nn in node.neighbours:
